1a019ded32ad5d6983c9629ca23dc79cf0864b59
RAW-si18 <intelligencecosmos@gmail.com>
2024-02-29 19:30:51 +0530

    29_02_24

--- AI/raw_modules/extra_funcs.py
+++
@@ -1,9 +0,0 @@
-# Web browser with url
-import webbrowser
-from raw_modules.speak_func import speak
-
-def webopen(url: str) -> None:
-    '''open website with particular url'''
-    webbrowser.open(url)
-    speak("Opening website.")
-    print('Opening website')
--- AI/raw_modules/similar_word.py
+++
@@ -1,7 +0,0 @@
-# Updated 23/1/24
-
-from difflib import get_close_matches
-
-def match_word_func(word: str, word_list: list) -> list:
-    matches = get_close_matches(word, word_list)
-    return matches
